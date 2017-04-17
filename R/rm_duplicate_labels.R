2a3d4fba6891a2cfd422eabc32e928e30a816142
ewenharrison <mail@ewenharrison.com>
2017-04-17 23:20:25 +0100

    OR plot manual

--- R/rm_duplicate_labels.R
+++ R/rm_duplicate_labels.R
@@ -1,6 +1,6 @@
 # Remove duplicate levels from summary.factorlist ----
-rm_duplicate_labels = function(summary.factorlist.output, na.to.missing = FALSE){
-	x = summary.factorlist.output
+rm_duplicate_labels = function(factorlist, na.to.missing = FALSE){
+	x = factorlist
 	duplicate_rows = duplicated(x$label)
 	x$label = as.character(x$label)
 	x$label[duplicate_rows] = ""
