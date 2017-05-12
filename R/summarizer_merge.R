5276485ec555ee0a322f8885411af134f791d34f
ewenharrison <mail@ewenharrison.com>
2017-05-12 15:09:49 +0100

    Summarzier merge ref.symbol argument works well, only when full model.

--- R/summarizer_merge.R
+++ R/summarizer_merge.R
@@ -1,7 +1,7 @@
 summarizer.merge = function(summary.out, glm.out, ref.symbol = "-"){
 	df.out = merge(summary.out, glm.out, by.x = "glm.id", by.y = "explanatory", all.x = TRUE)
-	df.out$OR = as.character(df.out$OR)
-	df.out$OR[is.na(df.out$OR)] = ref.symbol
+	df.out[, apply(df.out, 2, anyNA)] = as.character(df.out[, apply(df.out, 2, anyNA)]) # Change OR column with missings to character
+	df.out[is.na(df.out)] = ref.symbol
 	df.out = df.out[order(df.out$index),]
 	return(df.out)
 }
