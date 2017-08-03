0724ccd285b772940dd9b72a1b0c9eed38cc3b9e
ewenharrison <mail@ewenharrison.com>
2017-08-03 23:44:17 +0100

    interactions

--- R/summarizer_merge.R
+++ R/summarizer_merge.R
@@ -1,6 +1,6 @@
 summarizer.merge = function(factorlist, glm.df, ref.symbol = "-"){
 	or_col = grep("OR|HR", names(glm.df), value=TRUE)
-	df.out = merge(factorlist, glm.df, by.x = "glm.id", by.y = "explanatory", all.x = TRUE)
+	df.out = merge(factorlist, glm.df, by.x = "glm.id", by.y = "explanatory", all = TRUE)
 	df.out[,or_col] = as.character(df.out[,or_col])
 	df.out[is.na(df.out[,or_col]),or_col] = ref.symbol
 	df.out = df.out[order(df.out$index),]
