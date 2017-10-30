23011291f7880ff80545a6b82db6692b25fb37a0
ewenharrison <mail@ewenharrison.com>
2017-10-30 13:15:45 +0000

    Permissions change commit + single level factor

--- R/summaryfactorlist.R
+++ R/summaryfactorlist.R
@@ -1,25 +1,45 @@
-summary.factorlist <- function(df, dependent, explanatory, cont="mean", p=FALSE, na.include=FALSE,
+summary.factorlist <- function(df, dependent=NULL, explanatory, cont="mean", p=FALSE, na.include=FALSE,
 															 column=FALSE, total_col=FALSE, orderbytotal=FALSE, glm.id=FALSE,
 															 na.to.missing = TRUE){
 	try(if(is.data.frame(df)==FALSE) stop("df is not dataframe"))
-	try(if(is.null(dependent)) stop("No dependent variable(s) provided"))
 	try(if(is.null(explanatory)) stop("No explanatory variable(s) provided"))
+	if(is.null(dependent)){
+		warning("No dependent variable(s) provided; defaulting to single-level factor")
+		dependent = "all"
+		df$all = factor(1, label="all")
+	}
 
 	args = list(df=df, dependent=dependent, explanatory=explanatory, cont=cont, p=p, na.include=na.include,
 							column=column, total_col=total_col, orderbytotal=orderbytotal, glm.id=glm.id,
 							na.to.missing=na.to.missing)
 
-	d.len = length(levels(df[,names(df) %in% dependent]))
-	if (d.len == 0){
+	# Extract dependent variable
+	d.variable = df[,names(df) %in% dependent]
+
+	# Logical is.factor
+	d.isfactor = is.factor(d.variable) |
+		is.character(d.variable)
+
+	# Number of levels of dependent
+	d.len = length(levels(d.variable))
+
+	# Non-factor case
+	if(!d.isfactor){
 		warning("Dependent is not a factor and will be treated as a continuous variable")
-		do.call(summary.factorlist1, args)
-	} else if (d.len == 2){
-		do.call(summary.factorlist2, args)
-	} else if (d.len == 3){
-		do.call(summary.factorlist3, args)
-	} else if (d.len == 4){
-		do.call(summary.factorlist4, args)
-	} else if (d.len == 5){
-		do.call(summary.factorlist5, args)
+		do.call(summary.factorlist0, args)
+	} else {
+
+	# Factor case
+		if (d.len == 1){
+			do.call(summary.factorlist1, args)
+		} else if (d.len == 2){
+			do.call(summary.factorlist2, args)
+		} else if (d.len == 3){
+			do.call(summary.factorlist3, args)
+		} else if (d.len == 4){
+			do.call(summary.factorlist4, args)
+		} else if (d.len == 5){
+			do.call(summary.factorlist5, args)
+		}
 	}
 }
--- R/summaryfactorlist1.R
+++ R/summaryfactorlist1.R
@@ -1,38 +1,99 @@
-# Continuous dependent. 
 summary.factorlist1 <- function(df, dependent, explanatory,  cont="mean", p=FALSE, na.include=FALSE,
 															 column=FALSE, total_col=FALSE, orderbytotal=FALSE, glm.id=FALSE,
 																na.to.missing = TRUE){
-	
-	s = Hmisc::summary.formula(as.formula(paste(dependent, "~", paste(explanatory, collapse="+"))), data = df,
-															overall=FALSE, method="response", na.include=na.include, 
-															fun=function(x) {
-																mean = mean(x)
-																sd = sd(x)
-																L = quantile(x, probs=c(0.25))[[1]]
-																median = quantile(x, probs=c(0.5))[[1]]
-																U = quantile(x, probs=c(0.75))[[1]]
-																return(data.frame(mean, sd, L, median, U))
-															}
-	)
-	
-	# Dataframe
-	df.out = data.frame(label=attr(s, "vname"), level=attr(s, "dimnames")[[1]])
-	
-	if (cont=="mean"){
-		mean.out = sprintf("%.2f", matrix(s[,2]))
-		sd.out = sprintf("%.2f", matrix(s[,3]))
-		result.out = data.frame(paste0(mean.out, " (", sd.out, ")"))
-		colnames(result.out) = "Mean (sd)"
+
+	s <- Hmisc::summary.formula(as.formula(paste(dependent, "~", paste(explanatory, collapse="+"))), data = df,
+											 method="reverse", overall=FALSE,
+											 test=TRUE,na.include=na.include)
+	df.out = plyr::ldply(s$stats, function(x){
+		if(dim(x)[2] == 13){ #hack to get continuous vs categorical. Wouldn't work for factor with 13 levels
+			# Continuous variables
+			# Mean (SD)
+			if (cont == "mean"){
+				a = paste0(round(x[1,12], 1), " (", round(x[1,13], 1), ")")
+				row1_name = dimnames(x)[[2]][12]
+				row2_name = dimnames(x)[[2]][13]
+			}
+
+			# Median (IQR)
+			if (cont == "median"){
+				a = paste0(x[1,6], " (", x[1,8]-x[1,4], ")")
+				row1_name = "Median"
+				row2_name = "IQR"}
+
+			col1_name = dimnames(x)[[1]][1]
+			df.out = data.frame(paste0(row1_name, " (", row2_name, ")"), a)
+			names(df.out) = c("levels", col1_name)
+			return(df.out)
+
+		} else {
+			# Factor variables
+			row_name = dimnames(x)$w
+			col1_name = dimnames(x)$g[1]
+			col1 = x[,1]
+			total = col1
+			if (column == FALSE) {
+				col1_prop = (col1/apply(x, 1, sum))*100 # row margin
+			} else {
+				col1_prop = (col1/sum(col1))*100 # column margin
+				total_prop = (total/sum(total))*100
+			}
+			a = paste0(col1, " (", sprintf("%.1f", round(col1_prop, 1)), ")") #sprintf to keep trailing zeros
+			if (total_col == FALSE){
+				df.out = data.frame(row_name, a)
+				names(df.out) = c("levels", col1_name)
+			} else if (total_col == TRUE & column == FALSE) {
+				df.out = data.frame(row_name, a, total, total)
+				names(df.out) = c("levels", col1_name, "Total", "index_total")
+			} else if (total_col == TRUE & column == TRUE) {
+				df.out = data.frame(row_name, a, paste0(total, " (", sprintf("%.1f", round(total_prop, 1)), ")"), total)
+				names(df.out) = c("levels", col1_name, "Total", "index_total")
+			}
+		}
+		return(df.out)
+	})
+	# Keep original order
+	df.out$index = 1:dim(df.out)[1]
+
+	if (p == TRUE){
+		a = plyr::ldply(s$testresults, function(x) sprintf("%.3f",round(x[[1]], 3)))
+		names(a) = c(".id", "pvalue")
+		df.out = merge(df.out, a, by=".id")
 	}
-	
-	if (cont=="median"){
-		median.out = sprintf("%.2f", matrix(s[,5]))
-		L_IQR = sprintf("%.2f", matrix(s[,4]))
-		U_IQR = sprintf("%.2f", matrix(s[,6]))
-		result.out = data.frame(paste0(median.out, " (", L_IQR, " to ", U_IQR, ")"))
-		colnames(result.out) = "Median (IQR)"
+
+	# Add back in actual labels
+	df.labels = data.frame(".id" = names(s$stats), "label" = s$labels)
+	df.out.labels = merge(df.out, df.labels, by = ".id")
+	if (orderbytotal==FALSE){
+		df.out.labels = df.out.labels[order(df.out.labels$index),] # reorder columns
+	} else {
+		df.out.labels = df.out.labels[order(-df.out.labels$index_total),] # reorder columns
 	}
-	
-	df.out = cbind(df.out, result.out)
-	return(df.out)
+
+	# Reorder columns
+	label_index = which(names(df.out.labels) == "label")
+	not_label_index = which(names(df.out.labels) != "label")
+	df.out.labels = df.out.labels[,c(label_index,not_label_index)]
+
+	# Add glm levels name
+	if (glm.id){
+		levels = as.character(df.out.labels$levels)
+		levels[levels == "Mean (SD)"] = ""
+		levels[levels == "Median (IQR)"] = ""
+		df.out.labels$glm.id = paste0(df.out.labels$.id, levels)
+	}
+
+	# Remove
+	if (glm.id == FALSE) {
+		index_index = which(names(df.out.labels) == "index")
+	}else{
+		index_index = 0
+	}
+	id_index = which(names(df.out.labels) == ".id")
+	index_total_index = which(names(df.out.labels) == "index_total")
+	df.out.labels = df.out.labels[,-c(id_index, index_total_index, index_index)]
+
+	# Remove duplicate labels
+	df.out.labels = rm_duplicate_labels(df.out.labels, na.to.missing = na.to.missing)
+	return(df.out.labels)
 }
